91103a44a5e60f38128b19d216d89ca63c7caba2
shenqi329 <524607630@qq.com>
2014-09-28 22:31:43 +0800

    Initial Commit

---
+++ Triangle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Triangle
+//
+//  Created by shenqi329 on 9/28/14.
+//  Copyright (c) 2014 shenqi329. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Triangle/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Triangle
+//
+//  Created by shenqi329 on 9/28/14.
+//  Copyright (c) 2014 shenqi329. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
